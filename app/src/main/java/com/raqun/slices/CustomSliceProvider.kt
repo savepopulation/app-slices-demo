7960bb83ef79a4fa0f37633e6681db70c5e53a5c
tyln <taylan.sbrcn@gmail.com>
2018-10-31 07:59:01 +0300

    custom slice provider added

---
+++ app/src/main/java/com/raqun/slices/CustomSliceProvider.kt
@@ -0,0 +1,17 @@
+package com.raqun.slices
+
+import android.net.Uri
+import androidx.slice.Slice
+import androidx.slice.SliceProvider
+
+class CustomSliceProvider : SliceProvider() {
+
+    override fun onBindSlice(sliceUri: Uri?): Slice {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateSliceProvider(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
