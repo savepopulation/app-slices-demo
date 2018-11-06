32eb6d9aae8c1c59c95e541c350271cd2fb19edd
tyln <taylan.sbrcn@gmail.com>
2018-11-06 07:26:29 +0300

    slices handled

--- app/src/main/java/com/raqun/slices/CustomSliceProvider.kt
+++ app/src/main/java/com/raqun/slices/CustomSliceProvider.kt
@@ -1,15 +1,61 @@
 package com.raqun.slices
 
+import android.content.ContentResolver
+import android.content.Intent
 import android.net.Uri
 import androidx.slice.Slice
 import androidx.slice.SliceProvider
+import androidx.slice.builders.ListBuilder
+import androidx.slice.builders.list
+import androidx.slice.builders.row
 
 class CustomSliceProvider : SliceProvider() {
 
-    override fun onBindSlice(sliceUri: Uri?): Slice {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateSliceProvider(): Boolean = true
+
+    override fun onBindSlice(sliceUri: Uri): Slice? {
+
+        context?.let {
+            val sliceAction = SliceActionFactory.createHelloSlicesAction(context)
+
+            return if (sliceUri.path == "/slice") {
+                list(context, sliceUri, ListBuilder.INFINITY)
+                {
+                    row {
+                        title = "Hello Slices Demo"
+                        primaryAction = sliceAction
+                    }
+                }
+            } else {
+                list(context, sliceUri, ListBuilder.INFINITY)
+                {
+                    row {
+                        title = "Sorry! Uri not found!"
+                        primaryAction = sliceAction
+                    }
+                }
+            }
+        }
+
+        return null
     }
 
-    override fun onCreateSliceProvider(): Boolean = true
+    override fun onMapIntentToUri(intent: Intent?): Uri {
+        var uriBuilder: Uri.Builder = Uri.Builder().scheme(ContentResolver.SCHEME_CONTENT)
+
+        if (intent == null) return uriBuilder.build()
+
+        val data = intent.data
+        if (data != null && data.path != null) {
+            val path = data.path.replace("/slice", "")
+            uriBuilder = uriBuilder.path(path)
+        }
+
+        if (context != null) {
+            uriBuilder = uriBuilder.authority(context.packageName)
+        }
+
+        return uriBuilder.build()
+    }
 
 }
--- app/src/main/java/com/raqun/slices/SliceActionFactory.kt
+++ app/src/main/java/com/raqun/slices/SliceActionFactory.kt
@@ -11,16 +11,15 @@ class SliceActionFactory {
     companion object {
         fun createHelloSlicesAction(context: Context,
                                     requestCode: Int = 0,
-                                    flags: Int = 0) {
-            SliceAction.create(
-                    PendingIntent.getActivity(
-                            context,
-                            requestCode,
-                            Intent(context, MainActivity::class.java),
-                            flags),
-                    IconCompat.createWithResource(context, R.drawable.ic_launcher_foreground),
-                    ListBuilder.ICON_IMAGE,
-                    "Hello Slices")
-        }
+                                    flags: Int = 0) =
+                SliceAction.create(PendingIntent.getActivity(
+                        context,
+                        requestCode,
+                        Intent(context, MainActivity::class.java),
+                        flags),
+                        IconCompat.createWithResource(context, R.drawable.ic_launcher_foreground),
+                        ListBuilder.ICON_IMAGE,
+                        "Hello Slices")
+
     }
 }
